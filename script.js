2410c5393ee3a16cb5e08ad14aefa925fd835b14
Pranav Reddy <spranavreddy25@gmail.com>
2020-04-24 01:04:41 +0530

    Added delete

--- script.js
+++ script.js
@@ -25,18 +25,39 @@ function update()
 
 function add_todo(name)
 { 
-  total += 1
-  unchecked += 1
+  total += 1;
+  unchecked += 1;
   // update(total,unchecked);
-  let input =  document.createElement("input")
-  input.setAttribute("type","checkbox")
+  let input =  document.createElement("input");
+  input.setAttribute("type","checkbox");
 
   input.addEventListener("click",function(event) {
-    if(event.target){
-      unchecked -= 1
+    if(event.target.checked){
+      unchecked -= 1;
+      // node = node.strike();
     }
     else{
-      unchecked += 1
+      unchecked += 1;
+    }
+    update();
+  });
+
+  let remove = document.createElement("button");
+  let bname = document.createTextNode("Delete")
+  remove.append(bname)
+
+  remove.addEventListener("click",function(event) {
+    if(todo.childNodes[0].checked)
+    {
+      todo.parentNode.removeChild(todo);
+      // unchecked -= 1
+      total -= 1
+    }
+    else
+    {
+      todo.parentNode.removeChild(todo);
+      unchecked -= 1
+      total -= 1
     }
     update()
   })
@@ -45,6 +66,8 @@ function add_todo(name)
   let node = document.createTextNode(name)
   todo.append(input)
   todo.append(node)
+  todo.append(remove)
+  todo.append(document.createElement("hr"))
   list.append(todo)
   update()
 
