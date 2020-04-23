86d722c16e904b4f40fa854011c19f8b9b110073
Pranav Reddy <spranavreddy25@gmail.com>
2020-04-23 21:30:21 +0530

    Added checkbox

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ function newTodo() {
   add_todo(new_todo)
 }
 
-function update(total,unchecked)
+function update()
 {
   itemCountSpan.innerHTML = "" + total
   uncheckedCountSpan.innerHTML = "" + unchecked
@@ -27,9 +27,25 @@ function add_todo(name)
 { 
   total += 1
   unchecked += 1
-  update(total,unchecked);
+  // update(total,unchecked);
+  let input =  document.createElement("input")
+  input.setAttribute("type","checkbox")
+
+  input.addEventListener("click",function(event) {
+    if(event.target){
+      unchecked -= 1
+    }
+    else{
+      unchecked += 1
+    }
+    update()
+  })
+
   let todo = document.createElement("li")
   let node = document.createTextNode(name)
+  todo.append(input)
   todo.append(node)
   list.append(todo)
+  update()
+
 }
