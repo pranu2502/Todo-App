1376c1d6b41c1d9ddc933fab26320b37878ac264
Pranav Reddy <spranavreddy25@gmail.com>
2020-04-24 12:49:35 +0530

    added classes

--- script.js
+++ script.js
@@ -4,15 +4,14 @@ const classNames = {
   TODO_TEXT: 'todo-text',
   TODO_DELETE: 'todo-delete',
 }
-var total = 0
-var unchecked = 0
+let total = 0
+let  unchecked = 0
 const list = document.getElementById('todo-list')
 const itemCountSpan = document.getElementById('item-count')
 const uncheckedCountSpan = document.getElementById('unchecked-count')
 
 function newTodo() {
   let new_todo = prompt("Enter your new task")
-
   add_todo(new_todo)
 }
 
@@ -27,14 +26,13 @@ function add_todo(name)
 { 
   total += 1;
   unchecked += 1;
-  // update(total,unchecked);
+
   let input =  document.createElement("input");
   input.setAttribute("type","checkbox");
-
+  input.classList.add("todo-checkbox")
   input.addEventListener("click",function(event) {
     if(event.target.checked){
       unchecked -= 1;
-      // node = node.strike();
     }
     else{
       unchecked += 1;
@@ -43,14 +41,14 @@ function add_todo(name)
   });
 
   let remove = document.createElement("button");
-  let bname = document.createTextNode("Delete")
+  let bname = document.createTextNode("Delete");
+  remove.classList.add("todo-delete")
   remove.append(bname)
 
   remove.addEventListener("click",function(event) {
     if(todo.childNodes[0].checked)
     {
       todo.parentNode.removeChild(todo);
-      // unchecked -= 1
       total -= 1
     }
     else
@@ -64,11 +62,13 @@ function add_todo(name)
 
   let todo = document.createElement("li")
   let node = document.createTextNode(name)
+  todo.classList.add("todo-text")
   todo.append(input)
   todo.append(node)
   todo.append(remove)
   todo.append(document.createElement("hr"))
   list.append(todo)
+  list.classList.add("todo-container")
   update()
 
 }
